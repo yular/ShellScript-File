2b95fef120a1fffeda4c3c139738c32bf3d925d6
Baichuan Yang <1062969706@qq.com>
2016-01-11 15:31:53 -0800

    Update vim_find_and_replace_all_text_substitue_command.sh

--- vim_find_and_replace_all_text_substitue_command.sh
+++ vim_find_and_replace_all_text_substitue_command.sh
@@ -1,4 +1,5 @@
 # http://www.cyberciti.biz/faq/vim-text-editor-find-and-replace-all-text/
+
 :s/Search-Word/Replace-Word/  # Only change the search-word to replace-word behind the cursor in the same line
 :s/Search-Word/Replace-Word/g # The same as above
 :%s/Search-Word/Replace-Word/g # Change all search-words to replace-words without asking permission
