6f9a12bb0b9af5413856a236a1340541c4648162
Baichuan Yang <1062969706@qq.com>
2016-01-31 19:05:50 -0800

    Update timer_process.sh

--- timer_process.sh
+++ timer_process.sh
@@ -5,7 +5,7 @@ do
         time=$(date +%Y%m%d%H%M)
         H=${time:8:2}
         M=${time:10:2}
-        if [ "$H" == "09" ] && [ "$M" == "00" ]
+        if [ "$H" == "10" ] && [ "$M" == "30" ] #Run some commands on 10:30 am every day
         then
                 #do sth
         fi
