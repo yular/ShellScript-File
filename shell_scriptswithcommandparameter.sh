4943a1abf7431bfee1870dea78255b25f515e71c
Baichuan Yang <1062969706@qq.com>
2016-04-29 15:58:35 -0700

    Create shell_scriptswithcommandparameter.sh

---
+++ shell_scriptswithcommandparameter.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+usage="$(basename $0) -i <input var>  -o <output var>"
+
+echo $usage
+
+while getopts "i:o:" arg
+    do
+        case ${arg} in
+            i)
+                INPUT=${OPTARG}
+                ;;
+            o)
+                OUTPUT=${OPTARG}
+                ;;
+            \?)
+                echo "Invalid option: -${OPTARG}"
+                echo "${usage}"
+                exit 1
+                ;;
+            :)
+                echo "Option -${OPTARG} requires an argument"
+                echo "${usage}"
+                exit 1
+                ;;
+        esac
+    done
+
+echo "Input is "${INPUT}" and Output is "${OUTPUT}
