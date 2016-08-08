4e7031beadb523e38825443b68d875122ca29956
Baichuan Yang <1062969706@qq.com>
2016-08-08 11:15:15 -0700

    Create shell_crontab_edit.sh

---
+++ shell_crontab_edit.sh
@@ -0,0 +1,10 @@
+crontab -e
+
+1 2 3 4 5 /path/to/script
+
+#1: Minutes (0-59)
+#2: Hours (0-23)
+#3: Days (1-31)
+#4: Month (1-12)
+#5: Day of the week(1-7/0-6 (sunday 0))
+# https://www.debian-tutorials.com/crontab-tutorial-cron-howto
